9bb79679c4fe4d9761cb848657ee7735bfd771aa
beepscore <support@beepscore.com>
2012-09-21 14:44:02 -0700

    Move method description comments from .m to .h file. Reformat.

--- iOS examples/AGLKit/AGLKContext.h
+++ iOS examples/AGLKit/AGLKContext.h
@@ -7,8 +7,14 @@
 
 @interface AGLKContext : EAGLContext
 
+// clear (background) RGBA color.
+// The clear color is undefined until it is set.
 @property (nonatomic, assign, readwrite) GLKVector4 clearColor;
 
+// This method instructs OpenGL ES to set all data in the
+// current Context's Render Buffer(s) identified by mask to
+// colors (values) specified via -setClearColor: and/or
+// OpenGL ES functions for each Render Buffer type.
 - (void)clear:(GLbitfield)mask;
 
 - (void)enable:(GLenum)capability;
